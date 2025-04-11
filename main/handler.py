b52068d55704d25bb6e7d9d5a27acb2b5c21bbe5
Tim123456 <156076725+TimLL123456@users.noreply.github.com>
2025-04-11 16:20:17 +0800

    Update handler.py

--- main/handler.py
+++ main/handler.py
@@ -1,4 +1,7 @@
-# Better Code Structure: https://x.com/i/grok/share/qOrvNKRmWUAn4tSFCIJ1I2gHy
+# Better Code Structure:
+# Method 1: https://grok.com/share/bGVnYWN5_02f0506a-bf85-4091-97b3-96de0e5b9b0a
+# Method 2: https://x.com/i/grok/share/qOrvNKRmWUAn4tSFCIJ1I2gHy
+
 
 from telegram import (
     Update,
@@ -23,9 +26,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     user = update.effective_user
     user_id = user.id
+    user_username = user.username
+    user_lastname = user.last_name
+    user_firstname = user.first_name
 
-    print()
-    
+    print(f"get user by id: {db.get_user_by_id(user_id)}")
+
+    if db.get_user_by_id(user_id):
+        pass
+    else:
+        db.add_user(user_id, user_username, user_lastname, user_firstname)
+        print((user_id, user_username, user_lastname, user_firstname))
+
+
     greeting_message = (
         "Hello! 👋 <b>Welcome to your Personal Expense Manager Bot</b>. 💰\n\n"
         "I'm here to help you manage your finances easily.\n\n"
