ac2b718ff6f00e2050bda39e95b618873f91a53d
$uname <$lamlung001014@gmail.com>
2025-07-10 06:28:43 +0000

    Try logging

--- scr/config.py
+++ scr/config.py
@@ -1,7 +1,12 @@
+from custom_logger import *
+
 import os
 import logging
 from dotenv import load_dotenv
 
+logger = logging.getLogger("app.config")
+
+@log_function(logger)
 class Config:
     def __init__(self):
 
@@ -33,31 +38,3 @@ class Config:
         self.TELEGRAM_BOT_TOKEN = required_vars["TELEGRAM_BOT_TOKEN"]
         self.SUPABASE_URL = required_vars["SUPABASE_URL"]
         self.SUPABASE_KEY = required_vars["SUPABASE_KEY"]
-
-        # Configure logging
-        self.logger = self._setup_logger()
-
-    def _setup_logger(self):
-        """Sets up a logger with file and console handlers."""
-        logger = logging.getLogger('bookkeeping_bot')
-        logger.setLevel(logging.debug)
-
-        # File handler
-        file_handler = logging.FileHandler('app.log')
-        file_handler.setLevel(logging.DEBUG)
-        file_formatter = logging.Formatter(
-            '%(asctime)s - %(module)s - %(name)s - %(levelname)s - %(message)s'
-        )
-        file_handler.setFormatter(file_formatter)
-        logger.addHandler(file_handler)
-
-        # Console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        console_formatter = logging.Formatter(
-            '%(module)s - %(name)s - %(levelname)s - %(message)s'
-        )
-        console_handler.setFormatter(console_formatter)
-        logger.addHandler(console_handler)
-
-        return logger
--- scr/main.py
+++ scr/main.py
@@ -1,9 +1,31 @@
 from config import Config
+from custom_logger import *
 
 import logging
+from flask_ngrok import run_with_ngrok
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+run_with_ngrok(app)  # Starts ngrok when app runs
+
+# Set Logger
+logger = logger_setup(logger_name="app", logger_filename="app.log")
+logger = logging.getLogger("app.main")
+
+
+@app.route('/', methods=['GET'])
+@log_function(logger)
 def main():
-    pass
+
+    config = Config()
+
+    logger.info("Telegram Bot Running")
+    data = {"message": "Hello World"}
+    return jsonify(data)
+  
 
 if __name__ == "__main__":
-    main()
+    try:
+        app.run()
+    except Exception as e:
+        logger.error(e)
